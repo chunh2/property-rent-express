789a80a93cdfb6f6de733989be3cd9a5fa6827db
CH <chunhwa5138@outlook.com>
2025-02-19 11:44:38 +0800

    Create users table

---
+++ migrations/20250219031348-create-user.js
@@ -0,0 +1,46 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("users", {
+      user_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.BIGINT,
+      },
+      name: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      phone: {
+        type: Sequelize.STRING(20),
+        allowNull: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal(
+          "CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"
+        ),
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("users");
+  },
+};
---
+++ models/user.js
@@ -0,0 +1,73 @@
+"use strict";
+const { Model } = require("sequelize");
+const { hashPassword } = require("../utils/password");
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init(
+    {
+      user_id: {
+        type: DataTypes.BIGINT,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING(30),
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING(100),
+        unique: true,
+        allowNull: false,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      phone: {
+        type: DataTypes.STRING(20),
+        allowNull: true,
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+      tableName: "users",
+      timestamps: true,
+      defaultScope: {
+        attributes: {
+          exclude: ["password"],
+        },
+      },
+      scopes: {
+        withPassword: {
+          attributes: { include: ["password"] },
+        },
+      },
+      hooks: {
+        beforeCreate: async (user) => {
+          user.password = await hashPassword(user.password);
+        },
+        beforeUpdate: async (user) => {
+          if (user.changed("password")) {
+            user.password = await hashPassword(user.password);
+          }
+        },
+      },
+    }
+  );
+  return User;
+};
