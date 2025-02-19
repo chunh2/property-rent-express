1047625ef18dbcd9fc9e2cb34c6b1a8de28ac7e6
CH <chunhwa5138@outlook.com>
2025-02-19 16:13:14 +0800

    Add centralized router

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const router = require("./routes/router");
 
 dotenv.config();
 
@@ -12,3 +13,15 @@ const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => console.log(`Listening PORT ${PORT}`));
 
 // Server
+
+// Middleware
+
+app.use(express.json());
+
+// Middleware
+
+// Route
+
+app.use("/api", router);
+
+// Route
