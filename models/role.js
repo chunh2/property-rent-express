88ce9bcac6b867bd54f4c8be13ed9ee3dca34c44
CH <chunhwa5138@outlook.com>
2025-02-19 12:25:18 +0800

    Create table roles

---
+++ models/role.js
@@ -0,0 +1,36 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Role extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Role.init(
+    {
+      role_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      role_name: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+        unique: true,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Role",
+      tableName: "roles",
+      timestamps: true,
+    }
+  );
+  return Role;
+};
