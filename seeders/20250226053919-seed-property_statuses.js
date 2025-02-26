6c1853e6d223b2bf18f52b21a9fc597ac913ac20
CH <chunhwa5138@outlook.com>
2025-02-26 13:44:39 +0800

    Create seed for property_statuses

---
+++ seeders/20250226053919-seed-property_statuses.js
@@ -0,0 +1,40 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert("property_statuses", [
+      {
+        name: "available",
+      },
+      {
+        name: "pending",
+      },
+      {
+        name: "rented",
+      },
+      {
+        name: "unavailable",
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("property_statuses", null, {});
+  },
+};
