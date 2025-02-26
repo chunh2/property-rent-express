c1e4ccc75201f87c0132923a3eebe46b2c09dffd
CH <chunhwa5138@outlook.com>
2025-02-26 13:44:24 +0800

    Create table property_statuses

---
+++ models/propertystatus.js
@@ -0,0 +1,37 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class PropertyStatus extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  PropertyStatus.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+    },
+    {
+      sequelize,
+      modelName: "PropertyStatus",
+      tableName: "property_statuses",
+      timestamps: true,
+      updatedAt: false,
+    }
+  );
+  return PropertyStatus;
+};
