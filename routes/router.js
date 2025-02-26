12ba1d7e77166e8c87199168195afb35067514d2
CH <chunhwa5138@outlook.com>
2025-02-26 15:35:21 +0800

    Add propertyOwnerRouter

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 const authRoutes = require("./authRoutes");
+const propertyOwnerRouter = require("./propertyOwnerRoutes");
 
 const router = Router();
 
 router.use("/", authRoutes);
+router.use("/", propertyOwnerRouter);
 
 module.exports = router;
