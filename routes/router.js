bc0107a0430b338f48eba1dae4c4f8e459e82a5c
CH <chunhwa5138@outlook.com>
2025-02-28 23:48:15 +0800

    Added router for property type

--- routes/router.js
+++ routes/router.js
@@ -2,11 +2,13 @@ const { Router } = require("express");
 const authRoutes = require("./authRoutes");
 const propertyOwnerRouter = require("./propertyOwnerRoutes");
 const stateRouter = require("./stateRoutes");
+const propertyTypeRouter = require("./propertyTypeRoutes");
 
 const router = Router();
 
 router.use("/", authRoutes);
 router.use("/owner/", propertyOwnerRouter);
 router.use("/", stateRouter);
+router.use("/", propertyTypeRouter);
 
 module.exports = router;
